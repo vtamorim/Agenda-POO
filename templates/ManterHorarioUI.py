a7ed5c09db80d1a2967061e079e7a6d643b1c55c
Victor Miguel <vthero89@gmail.com>
2025-09-23 14:53:12 +0000

    Terminando o método listar

--- templates/ManterHorarioUI.py
+++ templates/ManterHorarioUI.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 from viewhor import View
 import time
+import viewsc
+import views
 from datetime import datetime
 
 class HorarioUI:
@@ -16,4 +18,14 @@ class HorarioUI:
         with tab4: HorarioUI.excluir()
     def listar():
         horarios = View.horario_listar()
-        
+        if len(horarios) == 0: st.write("Nenhum Cadastro Registrado")
+        else:
+            dic = []
+            for obj in horarios:
+                cliente = viewsc.View.Cliente_listar_id(obj.get_id_cliente())
+                servico = views.View.Servico_listar_id(obj.get_id_servico())
+                if cliente != None: cliente = cliente.get_nome()
+                if servico != None: servico = servico.get_nome()
+                dic.append({"id": obj.get_id(), "data": obj.get_data(),"confirmado":obj.get_confirmado(),"cliente" : cliente,"servico" : servico})
+            df = pd.DataFrame(dic)
+            st.dataframe(df)
